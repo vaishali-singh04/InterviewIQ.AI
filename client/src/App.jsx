9a7053ca325ac5d68d219e05b7b591f86fc80540
Vaishali Singh <vaishalisingh983927@gmail.com>
2026-06-16 12:11:53 +0530

    fix backend url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:8000"
+export const ServerUrl  = "http://interviewiq-ai-n1i6.onrender.com"
 
 function App() {
 
