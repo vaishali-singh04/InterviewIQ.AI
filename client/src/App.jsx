b49280e5bc5855055d573ec0da7fe68762ab8e76
Vaishali Singh <vaishalisingh983927@gmail.com>
2026-06-16 12:16:36 +0530

    fix https backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://interviewiq-ai-n1i6.onrender.com"
+export const ServerUrl  = "https://interviewiq-ai-n1i6.onrender.com"
 
 function App() {
 
