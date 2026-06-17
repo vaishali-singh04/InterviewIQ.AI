0dbc1172eea8c6542385a1f1636266330952158d
Vaishali Singh <vaishalisingh983927@gmail.com>
2026-06-17 12:00:49 +0530

    cookie fix

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -14,9 +14,9 @@ export const googleAuth = async (req,res) => {
         }
         let token = await genToken(user._id)
         res.cookie("token" , token , {
-            http:true,
+            httpOnly:true,
             secure:false,
-            sameSite:"strict",
+            sameSite:"Iax",
             maxAge:7 * 24 * 60 * 60 * 1000
         })
 
