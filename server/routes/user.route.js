efbb01a58db4f665ff71883619f5e160f46ae33d
Vaishali Singh <vaishalisingh983927@gmail.com>
2026-06-17 10:46:47 +0530

    test route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -6,5 +6,8 @@ import { getCurrentUser } from "../controllers/user.controller.js"
 const userRouter = express.Router()
 
 userRouter.get("/current-user", isAuth,getCurrentUser)
+userRouter.get("/test",(req,res)=>{
+   res.send("user route working")
+})
 
 export default userRouter
